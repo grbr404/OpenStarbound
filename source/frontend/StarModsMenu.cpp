ee9aaaaa04ef09e7ded75bd74ab5d90265a5e41e
grbr404 <166065505+grbr404@users.noreply.github.com>
2026-03-04 20:07:50 +0100

    Update StarModsMenu.cpp

--- source/frontend/StarModsMenu.cpp
+++ source/frontend/StarModsMenu.cpp
@@ -111,7 +111,7 @@ void ModsMenu::openLink() {
 
   auto& guiContext = GuiContext::singleton();
   if (auto desktopService = guiContext.applicationController()->desktopService())
-    m_applicationController->openUrl(link);
+    guiContext.applicationController()->openUrl(link);
   else
     guiContext.setClipboard(link);
 }
@@ -120,7 +120,7 @@ void ModsMenu::openWorkshop() {
   auto assets = Root::singleton().assets();
   auto& guiContext = GuiContext::singleton();
   if (auto desktopService = guiContext.applicationController()->desktopService())
-    m_applicationController->openUrl(assets->json("/interface/modsmenu/modsmenu.config:workshopLink").toString());
+    guiContext.applicationController()->openUrl(assets->json("/interface/modsmenu/modsmenu.config:workshopLink").toString());
 }
 
 }
