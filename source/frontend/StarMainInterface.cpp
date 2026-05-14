eb70996a133139c32c666021c609e2468c08d785
grbr404 <166065505+grbr404@users.noreply.github.com>
2026-05-14 19:16:10 +0200

    Update StarMainInterface.cpp

--- source/frontend/StarMainInterface.cpp
+++ source/frontend/StarMainInterface.cpp
@@ -141,7 +141,7 @@ MainInterface::MainInterface(UniverseClientPtr client, WorldPainterPtr painter, 
   m_paneManager.registerPane(MainInterfacePanes::QuestTracker, PaneLayer::Hud, m_questTracker);
 
   m_achievementsPane = make_shared<ScriptPane>(m_client, "/interface/scripted/achievements/achievementsgui.config");
-  m_paneManager.registerPane(MainInterfacePanes::Achievements, PaneLayer::Window, m_achievementsPane);
+  m_paneManager.registerPane(MainInterfacePanes::Achievements, PaneLayer::ModalWindow, m_achievementsPane);
 
   m_mmUpgrade = make_shared<ScriptPane>(m_client, Root::singleton().assets()->json("/interface.config:mainBar.mmUpgrade").getString("scriptPane", "/interface/scripted/mmupgrade/mmupgradegui.config"));
   m_paneManager.registerPane(MainInterfacePanes::MmUpgrade, PaneLayer::Window, m_mmUpgrade);
