4e1cdf69746c279734e371d7066c4abbe300961a
grbr404 <166065505+grbr404@users.noreply.github.com>
2026-07-05 17:51:41 +0200

    Update StarFarmableObject.cpp

--- source/game/objects/StarFarmableObject.cpp
+++ source/game/objects/StarFarmableObject.cpp
@@ -75,7 +75,7 @@ bool FarmableObject::harvest() {
       for (auto const& treasureItem : Root::singleton().treasureDatabase()->createTreasure(m_stages.get(m_stage).getString("harvestPool"), world()->threatLevel()))
         world()->addEntity(ItemDrop::createRandomizedDrop(treasureItem, position()));
     } catch (StarException const& e) {
-      Logger::warn("Invalid harvest pool '{}': {}", m_stages.get(m_stage).getString("harvestPool"), outputException(e, false));
+      Logger::warn("Invalid dropID in farmable object harvest. {}", outputException(e, false));
     }
 
     if (m_stages.get(m_stage).contains("resetToStage")) {
